8a59995152a250a79967236baac738e338529f88
Luiz Elias <luizeliasceem@gmail.com>
2019-04-11 23:54:40 -0300

    estrutura back-end

---
+++ src/controllers/BoxController.js
@@ -0,0 +1,18 @@
+const Box = require('../models/Box');
+
+class BoxController {
+  async store(req, res) {
+    const { title } = req.body;
+    const box = await Box.create({ title });
+    return res.send(box);
+  }
+  async show(req, res) {
+    const box = await Box.findById(req.params.id).populate({
+      path: `files`,
+      options: { sort: { createdAt: -1 } },
+    });
+    return res.send(box);
+  }
+}
+
+module.exports = new BoxController();
